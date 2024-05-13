b4d60a782d08fc0e979ff95a52b5d87c152ec58d
Tony <229899325@qq.com>
2024-05-13 17:12:31 +0800

    Update register.v

--- source/verilog_src/register.v
+++ source/verilog_src/register.v
@@ -9,6 +9,29 @@ input [31:0] WriteData,
 output[31:0] ReadData1,
 output[31:0]ReadData2
 };
+reg[31:0] regs[0:31];//register file
+integer i;
+always @(posedge clk or posedge rst) begin
+if(rst)
+begin
+for(i=0;i<=31;i=i+1)
+begin
+    reg[i]<=0;
+end
+//loop
+end//end of if part
 
+else 
+begin
+if(RegWrite&(wr!=0))
+begin
+regs[wr]<=WriteData;
+end
 
+end
+
+end
+//read part
+assign ReadData1=(rs1=5'b0)?0:regs[rs1];
+assign ReadData2=(rs2=5'b0)?0:regs[rs2];
 endmodule
