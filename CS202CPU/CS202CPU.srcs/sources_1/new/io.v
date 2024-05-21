56348ca2d755d887374838743981da62efa4ced5
Tony <229899325@qq.com>
2024-05-21 23:09:31 +0800

    Update io.v

--- CS202CPU/CS202CPU.srcs/sources_1/new/io.v
+++ CS202CPU/CS202CPU.srcs/sources_1/new/io.v
@@ -3,13 +3,23 @@ module io(
     input mWrite,
     input ioRead,
     input ioWrite,
+    input [31:0] addr_in,  // from alu_result in executs32 
     input[31:0]Mdata,//data from memory
-    input[31:0]Rdata,//data from register files
+    input[31:0]Rdata,//data from register files(idecode 32)
     input[15:0]kdata,//data from keyboard
     input[15:0]bdata,//data from button
     output[31:0]addr,//address to memory
-    output[31:0]r_data,//data to register files
-    output[31:0]w_data,//data to memory or Io
-
+    output [31:0]r_data,//data to register files
+    output reg [31:0]w_data//data to memory or Io
 );
+    assign addr=addr_in;
+    assign r_data=(ioRead==1)?{kdata,bdata}:Mdata;
+    always@* begin
+        if((mWrite==1)||(ioWrite==1))begin
+            w_data=Rdata;
+        end
+        else begin
+            w_data=32'hffffffff;
+        end
+    end
 endmodule
