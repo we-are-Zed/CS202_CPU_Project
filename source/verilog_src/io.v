7f1c1dd4a7f7f609d9deac1b22ce7fe77f550973
Tony <229899325@qq.com>
2024-05-25 23:37:10 +0800

    Update io.v

--- source/verilog_src/io.v
+++ source/verilog_src/io.v
@@ -9,14 +9,14 @@ module io(
     input[15:0]bdata,//data from ioread
     output[31:0]addr,//address to memory
     output [31:0]r_data,//data to register files
-    output reg [31:0]w_data//data to memory or Io
-    output LEDCtrl; // LED Chip Select
-    output SwitchCtrl; // Switch Chip Select
+    output reg [31:0]w_data,//data to memory or Io
+    output LEDCtrl, // LED Chip Select
+    output SwitchCtrl // Switch Chip Select
 );
     assign addr=addr_in;
     assign r_data=(ioRead==1)?{0,bdata}:Mdata;
-    assign LEDCtrl= (iowrite == 1'b1)?1'b1:1'b0; // led 模块的片选信号，高电平有效; 
-    assign SwitchCtrl= (ioread == 1'b1)?1'b1:1'b0; //switch 模块的片选信号，高电平有效;
+    assign LEDCtrl= (ioWrite == 1'b1)?1'b1:1'b0; // led 模块的片选信号，高电平有效; 
+    assign SwitchCtrl= (ioRead == 1'b1)?1'b1:1'b0; //switch 模块的片选信号，高电平有效;
     always@* begin
         if((mWrite==1)||(ioWrite==1))begin
             w_data=Rdata;
