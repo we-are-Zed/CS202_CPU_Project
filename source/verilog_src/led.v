843df7e6d496efaa401b2eaef6af580e356178ca
Zedd <126555263+we-are-Zed@users.noreply.github.com>
2024-06-02 01:00:22 +0800

    Update led.v
    Signed-off-by: Zedd <126555263+we-are-Zed@users.noreply.github.com>

--- source/verilog_src/led.v
+++ source/verilog_src/led.v
@@ -2,27 +2,28 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module leds (
-    input			ledrst,		// reset, active high (复位信号,高电平有效)
-    input			led_clk,	// clk for led (时钟信号)
-    input			ledwrite,	// led write enable, active high (写信号,高电平有效)
-    input			ledcs,		// 1 means the leds are selected as output (从memorio来的，由低至高位形成的LED片选信号)
-    input	[1:0]	ledaddr,	// 2'b00 means updata the low 16bits of ledout, 2'b10 means updata the high 8 bits of ledout
+    input			ledrst,		// reset, active high (澶嶄綅淇″彿,楂樼數骞虫湁鏁?)
+    input			led_clk,	// clk for led (鏃堕挓淇″彿)
+    input			ledwrite,	// led write enable, active high (鍐欎俊鍙?,楂樼數骞虫湁鏁?)
+    input			ledlow,		// 1 means the leds are selected as output 
+    input			ledmid,		// 1 means the leds are selected as output 
+    input			ledhigh,		// 1 means the leds are selected as output 
+    //input	[1:0]	ledaddr,	// 2'b00 means updata the low 16bits of ledout, 2'b10 means updata the high 8 bits of ledout
     input	[15:0]	ledwdata,	// the data (from register/memorio)  waiting for to be writen to the leds of the board
     output reg [23:0]	ledout		// the data writen to the leds  of the board
 );
   
     
-    always @ (posedge led_clk or posedge ledrst) begin
-        if (ledrst)
+    always @ (negedge led_clk or posedge ledrst) begin
+        if (!ledrst)
             ledout <= 24'h000000;
-		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		else if (ledcs && ledwrite) begin
-			if (ledaddr == 2'b00)
+		else if (ledlow||ledmid || ledhigh) begin
+			if (ledlow )
 				ledout[23:0] <= { ledout[23:16], ledwdata[15:0] };
-			else if (ledaddr == 2'b10 )
-				ledout[23:0] <= { ledwdata[7:0], ledout[15:0] };
-			else
-				ledout <= ledout;
+			else if (ledmid )
+				ledout[23:0] <= { ledout[23:16],ledwdata[7:0], ledout[7:0] };
+			else if(ledhigh)
+				ledout[23:0] <= {ledwdata[7:0], ledout[15:0] };
         end else begin
             ledout <= ledout;
         end
